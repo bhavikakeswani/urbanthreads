dcd859c355d8b00106b6c0181489b2ebc01e7bd1
Bhavika Keswani <bhavikakeswani79@gmail.com>
2025-08-28 16:33:26 +0530

    feat: fetch products from FakeStoreAPI and render on index.html & category.html

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+import requests
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FloatField, TextAreaField
@@ -10,9 +11,16 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 app = Flask(__name__)
 
+response = requests.get('https://fakestoreapi.com/products')
+products = response.json()
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    bestsellers_main = products[13:17]  
+    trending_now = products[6:9]
+    bestsellers = products[3:6]
+    featured_products = products[0:3]
+    return render_template('index.html',products=bestsellers_main, trending_now=trending_now, bestsellers=bestsellers, featured_products=featured_products)
 
 @app.route('/about')
 def about():
@@ -44,7 +52,7 @@ def privacy():
 
 @app.route('/categories')
 def category():
-    return render_template('category.html')
+    return render_template('category.html',products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
